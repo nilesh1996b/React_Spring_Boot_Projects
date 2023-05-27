552b810665f4ab4be30ecf7a4610be7291ad6c93
nilesh <nileshbind.nb@gmail.com>
2023-05-27 09:01:18 +0530

    employee_management_Project One

---
+++ 01_employee_management/01_Backend/src/test/java/com/nilesh/employee_management/EmployeeManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nilesh.employee_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
