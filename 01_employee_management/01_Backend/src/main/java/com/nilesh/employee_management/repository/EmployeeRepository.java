23eec1ab6f29b4e812b1ba27586ac009b44e8bc3
nilesh <nileshbind.nb@gmail.com>
2023-05-27 09:51:36 +0530

    react_created and added

---
+++ 01_employee_management/01_Backend/src/main/java/com/nilesh/employee_management/repository/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.nilesh.employee_management.repository;
+
+import com.nilesh.employee_management.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+}
